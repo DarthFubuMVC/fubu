7f07ea0921151c341d50288faec4e2ef6cbcb315
jeremydmiller <jeremydmiller@yahoo.com>
2014-02-04 07:43:35 -0600

    retrofitted some tests around the new MvcBuilder.  Defacto Closes GH-112, Closes GH-56

--- src/Fubu/Generation/RazorCommand.cs
+++ src/Fubu/Generation/RazorCommand.cs
@@ -3,11 +3,11 @@
 namespace Fubu.Generation
 {
     [CommandDescription("Creates and attaches a Spark view model/view pair to the project in the current folder")]
-    public class SparkCommand : FubuCommand<ViewInput>
+    public class RazorCommand : FubuCommand<ViewInput>
     {
         public override bool Execute(ViewInput input)
         {
-            input.TemplateFlag = input.TemplateFlag ?? "view.spark";
+            input.TemplateFlag = input.TemplateFlag ?? "view.cshtml";
 
             MvcBuilder.BuildView(input);
 
